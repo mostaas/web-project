eade76046c657b40c7cf1f3626aaa1be583906bf
faroukblz <laid.farouk.belazreg@gmail.com>
2025-05-17 23:01:43 +0100

    Update index.php

---
+++ travel-agency/public/index.php
@@ -0,0 +1,9 @@
+<?php include '../includes/header.php'; ?>
+
+<main>
+  <h2>Explore Our Travel Packages</h2>
+  <p>Book amazing trips to your dream destinations with our agency.</p>
+  <a href="/packages/list.php" class="btn">Browse Packages</a>
+</main>
+
+<?php include '../includes/footer.php'; ?>
