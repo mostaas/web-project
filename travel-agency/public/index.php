1505b2794a113e00a8b19856a7cfbe589c5717a7
faroukblz <laid.farouk.belazreg@gmail.com>
2025-05-17 23:46:48 +0100

    Update index.php

--- travel-agency/public/index.php
+++ travel-agency/public/index.php
@@ -3,7 +3,7 @@
 <main>
   <h2>Explore Our Travel Packages</h2>
   <p>Book amazing trips to your dream destinations with our agency.</p>
-  <a href="/packages/list.php" class="btn">Browse Packages</a>
+  <a href="../packages/list.php" class="btn">Browse Packages</a>
 </main>
 
 <?php include '../includes/footer.php'; ?>
