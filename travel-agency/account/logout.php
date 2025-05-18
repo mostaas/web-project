d5841f09aebfb1c80444a6372871dea796987abd
faroukblz <laid.farouk.belazreg@gmail.com>
2025-05-18 02:14:46 +0100

    Update logout.php

--- travel-agency/account/logout.php
+++ travel-agency/account/logout.php
@@ -1,12 +1,12 @@
 <!-- File: logout.php (project root) -->
 <?php
-require_once __DIR__ . '/includes/db.php';
+require_once __DIR__ . '/../includes/db.php';
 // Destroy session
 $_SESSION = [];
 if (ini_get("session.use_cookies")) {
     setcookie(session_name(), '', time() - 42000, '/');
 }
 session_destroy();
-header('Location: /public/index.php');
+header('Location: ../public/index.php');
 exit;
 ?>
