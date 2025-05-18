3e3406d993bf5554cb7ea5b788ed36b5dff53d1e
faroukblz <laid.farouk.belazreg@gmail.com>
2025-05-18 04:20:36 +0100

    Update footer.php

--- travel-agency/includes/footer.php
+++ travel-agency/includes/footer.php
@@ -1,6 +1,9 @@
- </main>
-  <footer class="footer">
-    <p>&copy; <?= date('Y') ?> Travel Agency. All rights reserved.</p>
-  </footer>
+</main>
+<footer class="footer" id="contact">
+  <p>&copy; <?= date('Y') ?> Travel Agency. All rights reserved.</p>
+  <p>Contact us: <a href="mailto:contact@travelagency.com">contact@travelagency.com</a>  
+     | Phone: +213 (663) 521‐305</p>
+  <small>12 stambouli, Mascara, Algeria</small>
+</footer>
 </body>
 </html>
