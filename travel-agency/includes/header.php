31985caabd611632ef43b3128c29892f880f6ef6
faroukblz <laid.farouk.belazreg@gmail.com>
2025-05-18 02:13:31 +0100

    Update header.php

--- travel-agency/includes/header.php
+++ travel-agency/includes/header.php
@@ -1,6 +1,9 @@
 <?php
 // File: includes/header.php
 // Session started in db.php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,12 +21,7 @@
     <div class="logo"><a href="../public/index.php">Travel Agency</a></div>
     <nav>
       <ul class="nav-links">
-      <?php if (empty($_SESSION['user_id'])): ?>
-        <li><a href="../public/index.php">Home</a></li>
-        <li><a href="../account/login.php">Login</a></li>
-        <li><a href="../account/register.php">Register</a></li>
-        <li><a href="/public/contact.php">Contact</a></li>
-      <?php endif; ?>
+      
       <?php if (!empty($_SESSION['user_id'])): ?>
         <li><a href="../public/index.php">Home</a></li>
         <li><a href="../packages/list.php">Packages</a></li>
@@ -31,6 +29,12 @@
         <li><a href="../account/reservations.php">My Reservations</a></li>
         <li><a href="../public/contact.php">Contact</a></li>
         <li><a href="../account/logout.php">Logout</a></li>
+      <?php endif; ?>
+      <?php if (empty($_SESSION['user_id'])): ?>
+        <li><a href="../public/index.php">Home</a></li>
+        <li><a href="../account/login.php">Login</a></li>
+        <li><a href="../account/register.php">Register</a></li>
+        <li><a href="/public/contact.php">Contact</a></li>
       <?php endif; ?>
           
         
